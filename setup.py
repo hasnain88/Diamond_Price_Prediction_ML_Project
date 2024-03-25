9a502f77298a5a4891d7fd7445308aad6ea64d90
Hasnain Narsandawala <hasnain.narsandawala@gmail.com>
2024-03-25 17:01:16 +0530

    Project Basic Structre

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
 from setuptools import find_packages, setup
 from typing import List
 
+HYPEN_E_DOT = '-e .'
 
 def get_reguirements(file_path:str)->List[str]:
+    '''
+    this function will return the list of requirements
+    '''
     requirements = []
     with open(file_path) as file_obj:
         requirements = file_obj.readlines()
         requirements = [req.replace("\n","") for req in requirements]
+        
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
         return requirements
 
 setup(
